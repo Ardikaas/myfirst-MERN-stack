d208a9275f0f05b13b2dabc44030e05c2040820a
Ardikaas <ardikaajisetiawan@gmail.com>
2023-10-01 10:05:42 +0700

    recommit

---
+++ client/src/component/Footer.js
@@ -0,0 +1,27 @@
+const Footer = () => {
+  return(
+    <div class="footer">
+      <h1>Ardikaas</h1>
+      <div class="footer-link">
+        <a href="https://www.instagram.com/ardikaas_/" target="_blank" rel="noreferrer">
+          <i class="fa-brands fa-instagram"></i>
+        </a>
+        <a href="https://github.com/Ardikaas" target="_blank" rel="noreferrer">
+          <i class="fa-brands fa-github"></i>
+        </a>
+        <a href="https://www.youtube.com/@ardikaaji" target="_blank" rel="noreferrer">
+          <i class="fa-brands fa-youtube"></i>
+        </a>
+        <a href="https://twitter.com/ar_as30" target="_blank" rel="noreferrer">
+          <i class="fa-brands fa-twitter"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/ardika-aji-setiawan/" target="_blank" rel="noreferrer">
+          <i class="fa-brands fa-linkedin"></i>
+        </a>
+      </div>
+      <p>&copy; 2023 Ardikaas.inc. All rights reserved</p>
+    </div>
+  )
+}
+
+export default Footer;
